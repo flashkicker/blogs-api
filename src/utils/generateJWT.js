db7736b12b96855cd0783de034a3906e83802f09
Ujjval <ujjvalkumaria@gmail.com>
2020-06-05 18:35:29 -0700

    fix jwt env variable

--- src/utils/generateJWT.js
+++ src/utils/generateJWT.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken"
 
 const generateJWT = (userId) => {
-	return jwt.sign({ userId }, "thisisasecret", {
+	return jwt.sign({ userId }, process.env.JWT_SECRET, {
 		expiresIn: "7 days",
 	})
 }
